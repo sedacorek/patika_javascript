81a834ebc2384c93a0a298477a38cf3c80f7b0f5
Seda Turker <sdturker@gmail.com>
2022-03-24 00:37:12 +0300

    js-odev1-tamamlandi

---
+++ js-odev1/js/clock.js
@@ -0,0 +1,25 @@
+
+let userName = prompt("Lütfen adınızı girin.")
+document.getElementById("myName").innerHTML = userName
+
+setInterval(showTime, 1000);
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let weekday = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
+    [new Date().getDay()]
+    
+  
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+    let currentTime = hour + ":" 
+            + min + ":" + sec + " " + weekday; 
+  
+    document.getElementById("myClock")
+            .innerHTML = currentTime;
+}
+showTime();
